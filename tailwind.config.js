5e7eaf0fc545a178ab77b29c67291a81d8908c3c
Alessandro Belli <belli@uni-bremen.de>
2024-06-03 20:39:58 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    content: [
+        './src/**/*.{html,js,hbs}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                anthracite: '#292C2F',
+                orangeValencia: {
+                    100: '#FEEAE6',
+                    200: '#FDC6BE',
+                    300: '#FBA296',
+                    400: '#F97D6E',
+                    500: '#F75946',
+                    600: '#D84727', // Default color
+                    700: '#A0361D',
+                    800: '#682513',
+                    900: '#301308'
+                }
+            },
+            fontFamily: {
+                heading: ['"Fira Mono"', 'monospace'],
+                body: ['"Lexend Deca"', 'sans-serif'],
+            },
+            scale: {
+                '101': '1.01',
+            }
+        },
+    },
+    plugins: [require('@tailwindcss/typography'),],
+};
